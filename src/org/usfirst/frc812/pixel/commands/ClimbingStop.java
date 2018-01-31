433bc61273f06eec6d8cbc32f84357ebd7bf560b
FRC812 <frc@team812.org>
2018-01-30 17:15:08 -0800

    rebuild from scratch

--- src/org/usfirst/frc812/pixel/commands/ClimbingStop.java
+++
@@ -1,9 +0,0 @@
-package org.usfirst.frc812.pixel.commands;
-
-import org.usfirst.frc812.pixel.Robot;
-
-public class ClimbingStop {
-	 protected void execute() {
-	    	Robot.ClimbingStop.stop();
-}
-}
